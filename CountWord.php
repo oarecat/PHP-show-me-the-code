56719db191816bf30551fef840396d508e0d3bd9
oarecat <826562617@qq.com>
2016-11-09 14:23:33 +0800

    2016.11.9：完成第4题，英文文本中单词出现的个数

---
+++ CountWord.php
@@ -0,0 +1,10 @@
+<?php 
+#第4题:任一个英文的纯文本文件，统计其中的单词出现的个数。
+function word_count($sentence){ 
+	$parrten = "/[a-zA-Z]+/";
+  	preg_match_all($parrten,$sentence,$arr,PREG_SET_ORDER);
+    return count($arr); 
+} 
+$word = file_get_contents("./content.txt");
+echo $word.'<br/>';
+echo word_count($word); 
